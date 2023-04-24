cb01959ba1119660fa0f1b71f6df3ba86a7dc19a
Bong <qhdgkdbs@gmail.com>
2023-04-24 17:16:51 +0900

    lecture2: 이미지 사전 로딩

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -12,6 +12,10 @@ function App() {
 
   useEffect(() => {
     const ImageModal = import('./components/ImageModal');
+
+    const img = new Image();
+    img.src =
+      'https://stillmed.olympic.org/media/Photos/2016/08/20/part-1/20-08-2016-Football-Men-01.jpg?interpolation=lanczos-none&resize=*:800';
   }, []);
 
   //   const handleMouseEnter = () => {
