4107e46f682922a36af44c1d7db52ea611bd4f5e
Bong <qhdgkdbs@gmail.com>
2023-04-28 16:56:39 +0900

    lecture3: 이미지 지연 로딩

--- lecture-3/src/components/Card.js
+++ lecture-3/src/components/Card.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const options = {};
+    const cb = (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.src = entry.target.dataset.src;
+          observer.unobserve(entry.target);
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(cb, options);
+    observer.observe(imgRef?.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={imgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
