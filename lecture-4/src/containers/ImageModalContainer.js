3051d21b2f2b87ad4639625314aee56734434192
Bong <qhdgkdbs@gmail.com>
2023-04-29 20:45:17 +0900

    lecture4: 이미지 모달 리렌더링 최적화

--- lecture-4/src/containers/ImageModalContainer.js
+++ lecture-4/src/containers/ImageModalContainer.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { shallowEqual, useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  // const { modalVisible, bgColor, src, alt } = useSelector(state => ({
-  //   modalVisible: state.imageModal.modalVisible,
-  //   bgColor: state.imageModal.bgColor,
-  //   src: state.imageModal.src,
-  //   alt: state.imageModal.alt,
-  // }));
+  const { modalVisible, bgColor, src, alt } = useSelector(
+    state => ({
+      modalVisible: state.imageModal.modalVisible,
+      bgColor: state.imageModal.bgColor,
+      src: state.imageModal.src,
+      alt: state.imageModal.alt,
+    }),
+    shallowEqual
+  );
 
-  const modalVisible = useSelector(state => state.imageModal.modalVisible);
-  const bgColor = useSelector(state => state.imageModal.bgColor);
-  const src = useSelector(state => state.imageModal.src);
-  const alt = useSelector(state => state.imageModal.alt);
+  // const modalVisible = useSelector(state => state.imageModal.modalVisible);
+  // const bgColor = useSelector(state => state.imageModal.bgColor);
+  // const src = useSelector(state => state.imageModal.src);
+  // const alt = useSelector(state => state.imageModal.alt);
 
   return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
