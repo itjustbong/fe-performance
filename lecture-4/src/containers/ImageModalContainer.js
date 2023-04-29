f5ac049edabb0111021ed309c6c13608a7917a57
Bong <qhdgkdbs@gmail.com>
2023-04-29 20:35:06 +0900

    lecture4: 객체를 반환하지 않고, 값을 반환하여 리덕스 최적화

--- lecture-4/src/containers/ImageModalContainer.js
+++ lecture-4/src/containers/ImageModalContainer.js
@@ -3,21 +3,19 @@ import { useSelector } from 'react-redux';
 import ImageModal from '../components/ImageModal';
 
 function ImageModalContainer() {
-  const { modalVisible, bgColor, src, alt } = useSelector(state => ({
-    modalVisible: state.imageModal.modalVisible,
-    bgColor: state.imageModal.bgColor,
-    src: state.imageModal.src,
-    alt: state.imageModal.alt,
-  }));
+  // const { modalVisible, bgColor, src, alt } = useSelector(state => ({
+  //   modalVisible: state.imageModal.modalVisible,
+  //   bgColor: state.imageModal.bgColor,
+  //   src: state.imageModal.src,
+  //   alt: state.imageModal.alt,
+  // }));
 
-  return (
-    <ImageModal
-      modalVisible={modalVisible}
-      bgColor={bgColor}
-      src={src}
-      alt={alt}
-    />
-  );
+  const modalVisible = useSelector(state => state.imageModal.modalVisible);
+  const bgColor = useSelector(state => state.imageModal.bgColor);
+  const src = useSelector(state => state.imageModal.src);
+  const alt = useSelector(state => state.imageModal.alt);
+
+  return <ImageModal modalVisible={modalVisible} bgColor={bgColor} src={src} alt={alt} />;
 }
 
 export default ImageModalContainer;
